7f091b5b4d57720d8566987ff85c8b0567b88b08
ryan bautista <96922150+xryanb@users.noreply.github.com>
2022-03-29 22:28:32 -0700

    Add files via upload

---
+++ dojoweather/script.js
@@ -0,0 +1,15 @@
+
+
+function tapHere(){
+alert('Loading weather report...');
+}
+
+
+function beGone(id){
+    var element=document.querySelector(id);
+    element.remove();
+}
+
+
+/* I havent done the challenge part..still alot for me to learn*/
+
